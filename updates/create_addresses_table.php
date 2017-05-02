948697006f3aaec2b140ef085caa75717d057102
Trias Nur Rahman <contact@triasrahman.com>
2017-05-02 23:09:07 +0700

    Set addresses columns as nullable

--- updates/create_addresses_table.php
+++ updates/create_addresses_table.php
@@ -12,11 +12,11 @@ class CreateAddressesTable extends Migration
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
-            $table->string('location_code', 15);
-            $table->string('address_name', 100);
-            $table->string('name', 100);
-            $table->string('phone', 15);
-            $table->text('street');
+            $table->string('location_code', 15)->nullable();
+            $table->string('address_name', 100)->nullable();
+            $table->string('name', 100)->nullable();
+            $table->string('phone', 15)->nullable();
+            $table->text('street')->nullable();
             $table->decimal('latitude', 11, 8)->nullable();
             $table->decimal('longitude', 11, 8)->nullable();
             $table->boolean('is_primary')->default(0);
