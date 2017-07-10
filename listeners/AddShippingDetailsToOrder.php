2ac60b832bf70d2ce83f66837d6d61bd547e0ed8
Surahman <surahman.duang@gmail.com>
2017-07-10 17:48:21 +0700

    Remove unused namespaces

--- listeners/AddShippingDetailsToOrder.php
+++ listeners/AddShippingDetailsToOrder.php
@@ -1,19 +1,15 @@
 <?php namespace Octommerce\Shipping\Listeners;
 
-use Prosehat\Wallet\Models\Voucher;
-use Prosehat\Wallet\Validation\Voucher\Classes\Validator;
-use Prosehat\Wallet\Validation\Campaign\Classes\Validator as CampaignValidator;
-
 class AddShippingDetailsToOrder 
 {
 
     public function handle($order, $data, $cart)
     {
-        if ($order->address) {
-            $order->shipping_address = $order->address->street;
-            $order->shipping_location_code = $order->address->location_code;
-            $order->shipping_latitude = $order->address->latitude;
-            $order->shipping_longitude = $order->address->longitude;
+        if ($order->shipping_address) {
+            $order->shipping_address = $order->shipping_address->street;
+            $order->shipping_location_code = $order->shipping_address->location_code;
+            $order->shipping_latitude = $order->shipping_address->latitude;
+            $order->shipping_longitude = $order->shipping_address->longitude;
 
             $order->save();
         }
