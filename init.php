1936e22921d080912464381374a37b6b61e73f2d
Surahman <surahman.duang@gmail.com>
2017-07-11 14:51:20 +0700

    Takeout listener to add shipping cost after creating order
    Add shipping cost to cart instead

--- init.php
+++ init.php
@@ -2,6 +2,5 @@
 
 Event::listen('order.beforeCreate', 'Octommerce\Shipping\Listeners\CheckIsShippingSelected');
 
-Event::listen('order.afterCreate', 'Octommerce\Shipping\Listeners\AddShippingCost');
 Event::listen('order.afterCreate', 'Octommerce\Shipping\Listeners\MarkAsCod');
 Event::listen('order.afterCreate', 'Octommerce\Shipping\Listeners\AddShippingDetailsToOrder');
