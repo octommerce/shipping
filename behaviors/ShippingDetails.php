c3cfd9b295aa5fe40254e83f158fa72b9fa6c859
Trias Nur Rahman <contact@triasrahman.com>
2017-07-16 09:36:17 +0700

    Update shipping address details

--- behaviors/ShippingDetails.php
+++ behaviors/ShippingDetails.php
@@ -15,11 +15,13 @@ class ShippingDetails extends \October\Rain\Extension\ExtensionBase
     {
         $address = Address::find($data['shipping_address_id']);
 
+        $this->parent->is_same_address = 0;
         $this->parent->shipping_address_id = $address->id;
         $this->parent->shipping_location_code = $address->location_code;
         $this->parent->shipping_latitude = $address->latitude;
         $this->parent->shipping_longitude = $address->longitude;
-        $this->parent->shipping_name = $address->name;
+        $this->parent->shipping_name = $address->phone ? $address->phone : $this->parent->name;
+        $this->parent->shipping_phone = $address->phone ? $address->phone : $this->parent->phone;
         $this->parent->shipping_address = $address->street;
         $this->parent->save();
     }
