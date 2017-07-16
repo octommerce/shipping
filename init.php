64ab8c4c5c9310062048abff1f2bf8d9d37d610f
Trias Nur Rahman <contact@triasrahman.com>
2017-07-16 23:37:31 +0700

    Add shipping cost to invoice

--- init.php
+++ init.php
@@ -4,3 +4,5 @@ Event::listen('order.beforeCreate', 'Octommerce\Shipping\Listeners\CheckIsShippi
 
 Event::listen('order.afterCreate', 'Octommerce\Shipping\Listeners\MarkAsCod');
 Event::listen('order.afterCreate', 'Octommerce\Shipping\Listeners\AddShippingDetailsToOrder');
+
+Event::listen('order.beforeAddInvoice', 'Octommerce\Shipping\Listeners\AddInvoiceItem');
